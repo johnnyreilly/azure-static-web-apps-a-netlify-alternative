b89ccd43d9b04523d6d3a063fc79da67796711ad
johnnyreilly <johnny_reilly@hotmail.com>
2022-01-01 20:02:21 +0000

    authn

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,40 +1,64 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+import React, { useState, useEffect } from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+/**
+ * @typedef {object} UserInfo
+ * @property {"github"} identityProvider
+ * @property {string} userId
+ * @property {string} userDetails
+ * @property {string[]} userRoles
+ */
+
+/**
+ * @return {UserInfo | null}
+ */
+function useUserInfo() {
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    async function getUserInfo() {
+      const response = await fetch("/.auth/me");
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    }
+
+    getUserInfo().then((ui) => setUserInfo(ui));
+  });
+
+  return userInfo;
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+  const userInfo = useUserInfo();
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            {userInfo ? (
+              <p>Hello {userInfo.userDetails}</p>
+            ) : (
+              <a
+                className="button button--secondary button--lg"
+                href="/.auth/login/github"
+              >
+                Click here to login
+              </a>
+            )}
+          </div>
+        </div>
+      </header>
     </Layout>
   );
 }
